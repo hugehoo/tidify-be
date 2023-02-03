4acc0e08e0d522946d07385a57454c9f8eafd915
hoohuge <sunghoo.lim@fitpet.com>
2023-02-03 12:55:14 +0900

    init

---
+++ src/main/java/tidify/tidify/TidifyApplication.java
@@ -0,0 +1,13 @@
+package tidify.tidify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TidifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tidify/tidify/TidifyApplicationTests.java
@@ -0,0 +1,13 @@
+package tidify.tidify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
