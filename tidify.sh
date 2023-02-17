2c9617dbc3405f9467123d2793a2adb57ba7383a
hoo <tbnsok40@gmail.com>
2023-02-17 16:36:30 +0900

    shell script

---
+++ tidify.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Clean the project
+./gradlew clean
+
+# Build the project
+./gradlew build
+
+# Set the Docker image name and tag
+DOCKER_IMAGE_NAME=tbnsok/tidify-be
+DOCKER_IMAGE_TAG=latest
+
+# Build the Docker image using buildx
+docker buildx build --platform linux/amd64 --load --tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG} .
+
+# Push the Docker image to the Docker Hub registry
+docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
