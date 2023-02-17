689b42deb9c716d1d99081886d97d02410ec0194
hoo <tbnsok40@gmail.com>
2023-02-17 16:41:34 +0900

    healthcheck api

---
+++ src/main/java/tidify/tidify/controller/BasicController.java
@@ -0,0 +1,16 @@
+package tidify.tidify.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class BasicController {
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok().body("Nexters TDFY 레쓰고");
+    }
+
+}
