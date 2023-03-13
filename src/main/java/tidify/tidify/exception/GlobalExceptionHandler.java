811c4edc62fbb7cf874ba2bcf5c7c577bfa162f4
hoo <tbnsok40@gmail.com>
2023-03-13 18:44:01 +0900

    feature: 예외처리 핸들러 추가

--- src/main/java/tidify/tidify/exception/GlobalExceptionHandler.java
+++ src/main/java/tidify/tidify/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package tidify.tidify.exception;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -18,5 +21,25 @@ public class GlobalExceptionHandler {
             .build();
         log.error("ResourceNotFoundException(): " + exceptionDto.toString());
         return new ResponseEntity<>(exceptionDto, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public final ResponseEntity<?> handleSQLViolationException(SQLIntegrityConstraintViolationException exception) {
+        ExceptionDto exceptionDto = ExceptionDto.builder()
+            .message(exception.getMessage())
+            .errorCode(ErrorTypes.SQL_VIOLATION_EXCEPTION.getCode())
+            .build();
+        log.error("SQLIntegrityConstraintViolationException(): " + exceptionDto.toString());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public final ResponseEntity<?> handleHttpValidationException(HttpMessageNotReadableException exception) {
+        ExceptionDto exceptionDto = ExceptionDto.builder()
+            .message(exception.getMessage())
+            .errorCode(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+            .build();
+        log.error("HttpMessageNotReadableException(): " + exceptionDto.toString());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.BAD_REQUEST);
     }
 }
