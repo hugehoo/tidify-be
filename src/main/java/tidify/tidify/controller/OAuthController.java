ea319503e03cbc48befa2ae9b74c16c63bd3e458
hoo <tbnsok40@gmail.com>
2023-08-17 21:40:49 +0900

    fix: OAuth 로그인 ResponseDto 통일

--- src/main/java/tidify/tidify/controller/OAuthController.java
+++ src/main/java/tidify/tidify/controller/OAuthController.java
@@ -5,13 +5,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import tidify.tidify.domain.User;
+import tidify.tidify.dto.ObjectResponseDto;
 import tidify.tidify.dto.ResponseDto;
 import tidify.tidify.security.Token;
 import tidify.tidify.domain.SocialType;
@@ -25,10 +25,11 @@ public class OAuthController {
     private final UserService userService;
 
     @GetMapping("login")
-    public Token loginOauth2(@RequestParam SocialType type,
+    public ObjectResponseDto<Token> loginOauth2(@RequestParam SocialType type,
         HttpServletRequest request) {
         String code = request.getHeader("Authorization");
-        return userService.getAuthenticate(code, type);
+        Token authenticate = userService.getAuthenticate(code, type);
+        return new ObjectResponseDto<>(authenticate);
     }
 
     @DeleteMapping("withdrawal")
--- src/main/java/tidify/tidify/domain/FolderSubscribe.java
+++ src/main/java/tidify/tidify/domain/FolderSubscribe.java
@@ -1,16 +1,9 @@
 package tidify.tidify.domain;
 
-import java.util.List;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
